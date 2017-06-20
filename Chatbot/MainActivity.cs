fa6d9e405adb876cac0b7b77909d508a7552c888
Thuan Nguyen <thuan.nguyen@unicorn.vn>
2017-06-20 14:29:03 +0700

    Add Bot Connector using Direct Line Package

--- Chatbot/MainActivity.cs
+++ Chatbot/MainActivity.cs
@@ -1,26 +1,60 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System.Collections.Generic;
+using Chatbot.Services;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+using Android.Support.V7.Widget;
 
 namespace Chatbot
 {
 	[Activity(Label = "Chatbot", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+		public EditText UserMessage { get; set; }
+		public ImageButton SendButton { get; set; }
+		public RecyclerView MessagesRecycler { get; set; }
+		public List<Microsoft.Bot.Connector.DirectLine.Activity> MessagesList { get; set; }
+		public BotConnector BotConnector { get; set; }
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
-
-			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
+			InitBotConnector(); 
+			InitViews();
+			InitEvents();
+		}
 
-			button.Click += delegate { button.Text = $"{count++} clicks!"; };
+		private void InitViews()
+		{
+			UserMessage = FindViewById<EditText>(Resource.Id.main_inputmessage_edittext);
+			SendButton = FindViewById<ImageButton>(Resource.Id.main_send_floatingactionbutton);
+			MessagesRecycler = FindViewById<RecyclerView>(Resource.Id.main_message_listview);
+		}
+
+		private async void InitBotConnector()
+		{
+			var id = Android.Provider.Settings.Secure.AndroidId;
+			BotConnector = new BotConnector(id);
+			await BotConnector.StartBotConversation();
+		}
+
+		private void InitEvents()
+		{
+			SendButton.Click += (sender, e) =>
+			{
+				BotConnector.SendMessage(UserMessage.Text).ContinueWith((arg) => 
+				{
+					BotConnector.GetMessages().ContinueWith(async (result) => 
+					{
+						var messages = await result;
+					});
+				});
+			};
 		}
 	}
 }
---
+++ Chatbot/Services/BotConnector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector.DirectLine;
+
+namespace Chatbot.Services
+{
+	/// <summary>
+	/// Bot connector
+	/// Follow http://kristianbrimble.com/direct-line-v3-and-the-new-direct-line-client/
+	/// </summary>
+	public class BotConnector
+	{
+		private readonly DirectLineClient _directLineClient;
+		private Conversation _conversation;
+		private string _watermark;
+
+		private string directLineSecret = "-oBb0OJjaaM.cwA.FzA.yQnOKaTnrr488oYLFAlYVDguZfBC3a93r69UXGhWAJo";
+		private string botId = "DontCareBot";
+		private string _fromUser = "ClientUser";
+
+		public BotConnector(string fromUser)
+		{
+			_fromUser = fromUser;
+			_directLineClient = new DirectLineClient(directLineSecret);
+		}
+
+		public async Task StartBotConversation()
+		{
+			_conversation = await _directLineClient.Conversations
+												   .StartConversationAsync()
+												   .ConfigureAwait(false);
+		}
+
+		public async Task SendMessage(string message)
+		{
+			var fromProperty = new ChannelAccount(_fromUser);
+			var activity = new Activity(text: message, fromProperty: fromProperty, type: "message");
+			await _directLineClient.Conversations
+								   .PostActivityAsync(_conversation.ConversationId, activity)
+								   .ConfigureAwait(false);
+		}
+
+		public async Task<IList<Activity>> GetMessages()
+		{
+			var response = await _directLineClient.Conversations
+												  .GetActivitiesAsync(_conversation.ConversationId, _watermark)
+												  .ConfigureAwait(false);
+			_watermark = response.Watermark;
+			return response.Activities;
+		}
+	}
+}
